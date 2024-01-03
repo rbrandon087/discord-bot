d9fd4757e469beb2831af91a6407d0e355d137a6
Brandon <rbrandon087@gmail.com>
2024-01-02 22:49:56 -0800

    added another intents on my Client to alllow my bot to respond to a mesage took of my token so it does not reset on github

--- app.js
+++ app.js
@@ -1,14 +1,27 @@
 const { Client, GatewayIntentBits } = require('discord.js');
-const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages] });
+const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent,] });
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}`);
 });
 
 client.on('messageCreate', (message) => {
-  if (message.content === '!isbotonline') {
-    message.reply('Yes, I am online!');
+  try {
+    console.log(`Received message: ${message.content}`);
+
+    if (message.author.bot || !message.content.startsWith('!')) {
+      return;
+    }
+
+    const [command, ...args] = message.content.slice(1).split(' ');
+
+    if (command === 'hello') {
+      console.log('Command matched: !hello');
+      message.reply('Good day sir! :)');
+    }
+  } catch (error) {
+    console.error('Error in messageCreate event handler:', error);
   }
 });
 
-client.login('MTE5MTEyNzEwNDY1MDIzNTk5Ng.GfrC0B.q6b8J7qK56RRY_k9EW1yoMyRTNYP4WZP9asbQg');
+client.login('MTE5MTEyNzEwNDY1MDIzNTk5Ng.GiSgFD.-iXu5uipLtDijozI7vIikop0rJrInpuvCDJKBE');
