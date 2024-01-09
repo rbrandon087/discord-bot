29c5d509a4057b04b1ad6345d1ea59b9452aa2b3
Brandon <rbrandon087@gmail.com>
2024-01-08 22:33:13 -0800

    still working on the weather api disord now uses EmbedBuilder with its new version

--- app.js
+++ app.js
@@ -1,23 +1,35 @@
 const { Client, GatewayIntentBits, EmbedBuilder } = require('discord.js');
-const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent,] });
+const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent] });
 const fetch = require('node-fetch');
 const axios = require('axios');
 
-
 const weatherStackApiKey = '3675aab822163044e0d91dc71330eb66';
+
+// Weather API here
+class WeatherEmbed {
+  constructor(cityName, weatherData) {
+    this.embed = new EmbedBuilder()
+      .setTitle(`Current Weather in ${cityName}`)
+      .addField('Temperature', `${weatherData.temperature} °C`, true)
+      .addField('Condition', weatherData.weather_descriptions[0], true)
+      .addField('Humidity', `${weatherData.humidity}%`, true)
+      .addField('Wind Speed', `${weatherData.wind_speed} m/s`, true);
+  }
+
+  getEmbed() {
+    return this.embed;
+  }
+}
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}`);
 });
-
-// Weather API here
 
 // Fetch API Quote
 function getQuote() {
   return fetch("https://type.fit/api/quotes")
     .then(res => res.json())
     .then(data => {
-
       console.log('API Response:', data);
 
       if (Array.isArray(data) && data.length > 0) {
@@ -40,7 +52,6 @@ function getQuote() {
     });
 }
 
-
 // Listen for commands
 client.on('messageCreate', async (message) => {
   try {
@@ -52,27 +63,26 @@ client.on('messageCreate', async (message) => {
 
     const [command, ...args] = message.content.slice(1).split(' ');
 
+    console.log(`Command: ${command}`);
+
     if (command === 'hello') {
       console.log('Command matched: !hello');
       message.reply('Good day sir! :)');
     } else if (command === 'quote') {
+      console.log('Command matched: !quote');
       getQuote().then((quote) => message.reply(quote));
     } else if (message.content.toLowerCase() === '!weather') {
+      console.log('Command matched: !weather');
       const cityName = 'Las Vegas';
 
       const response = await axios.get(
-        `http://api.weatherstack.com/current?access_key=${weatherStackApiKey}&query=${cityName}`
+        `http://api.weatherstack.com/current?access_key=3675aab822163044e0d91dc71330eb66&query=Las Vegas`
       );
       const weatherData = response.data.current;
 
-      const embed = new EmbedBuilder()
-        .setTitle(`Current Weather in ${cityName}`)
-        .addField('Temperature', `${weatherData.temperature} °C`, true)
-        .addField('Condition', weatherData.weather_descriptions[0], true)
-        .addField('Humidity', `${weatherData.humidity}%`, true)
-        .addField('Wind Speed', `${weatherData.wind_speed} m/s`, true);
-
-      message.channel.send(embed);
+      const weatherEmbed = new WeatherEmbed(cityName, weatherData);
+      message.channel.send({ embeds: [weatherEmbed.getEmbed()] });
+      
     }
   } catch (error) {
     console.error('Error in messageCreate event handler:', error);
@@ -80,4 +90,4 @@ client.on('messageCreate', async (message) => {
 });
 
 
-client.login('MTE5MTEyNzEwNDY1MDIzNTk5Ng.G70gPO.T4NLvz0BHWxQYwoVYex_jhQWKRQWmftdHFBzc8');
+client.login('MTE5MTEyNzEwNDY1MDIzNTk5Ng.GHHI_G.3k-3GV4w0O-NwfAIjgC8T01HSDQ-wycZBR6L3Q');
